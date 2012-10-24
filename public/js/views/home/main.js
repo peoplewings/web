ee46c3a7727490007a80d250957184f3750e6fd5
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-10-24 14:40:12 +0200

    Started some validation fun on this forms

--- public/js/views/home/main.js
+++ public/js/views/home/main.js
@@ -1,16 +1,39 @@
 define([
   'jquery',
   'backbone',
+  'utils',
   'text!templates/home/main.html',
   'text!templates/home/accomodation.html',
-], function($, Backbone, mainHomeTpl, accomodationTpl){
+], function($, Backbone, utils, mainHomeTpl, accomodationTpl){
 
   var mainHomeView = Backbone.View.extend({
     el: "#main",
+	events: {
+		"submit form#accomodation-form": "submitSearch",
+	},
     render: function(){
       $(this.el).html(mainHomeTpl);
       $("#accomodation").html(accomodationTpl)
-    }
+    },
+	submitSearch: function(e){
+		e.preventDefault()
+		console.log('Submit Search: #' + e.target.id)
+		//require(["jquery.Validate"], function(foo){
+		/*var val = $('#' + e.target.id).validate({
+				rules: {
+					inputWings: "required",
+					language: "required",
+				},
+				highlight: function(label) {
+		    		$(label).closest('.control-group').addClass('error');
+		    	},
+			})
+		console.log(val)*/
+			
+		//})
+		
+		
+	}
   });
   return new mainHomeView;
 });
--- public/js/views/home/register.js
+++ public/js/views/home/register.js
@@ -6,7 +6,8 @@ define([
   'views/lib/handlers',
   'views/lib/response',
   'text!templates/home/register.html',
-], function($, Backbone, api, utils, handlersV, responseView, registerTpl){
+  'jquery.Validate',
+], function($, Backbone, api, utils, handlersV, responseView, registerTpl, jValidator){
 	
   var registerView = Backbone.View.extend({
     el: "#main",
@@ -16,17 +17,23 @@ define([
 
     render: function(){
       $(this.el).html(registerTpl);
+      $('#register-form').validate()
     },
 
 	submitRegister: function(e){
 		e.preventDefault(e);
 		var data = utils.serializeForm(e.target.id)
-		//console.log(data)
+		console.log(data)
+		//require(["jquery.Validate"], function(jValidatior){
+			//$('#' + e.target.id).validate()
+			//console.log(v)
+		//})
+		
 		handlersV.submitForm(e, '/newuser', data, responseView, 
 			{ 
 				legend: "Confirm your e-mail address",
 				msg: "A confirmation email has been sent to <b><%- email %></b>. Click on the confirmation link in the email to activate your account",
-			})
+		})
 	}
 
   });
