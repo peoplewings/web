491eaeb147c8bd4ef9d5c95fb920859c2822484d
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-11-13 12:33:47 +0100

    Fixed email shown after registration bug

--- public/js/views/home/register.js
+++ public/js/views/home/register.js
@@ -22,10 +22,12 @@ define([
 	submitRegister: function(e){
 		e.preventDefault(e);
 		var data = utils.serializeForm(e.target.id)
+		console.log(data)
 		handlersV.submitForm(e.target.id, '/newuser', data, responseView, 
 			{ 
 				legend: "Confirm your e-mail address",
-				msg: "A confirmation email has been sent to <b><%- email %></b>. Click on the confirmation link in the email to activate your account",
+				msg: "A confirmation email has been sent to ",
+				extraInfo: data.email
 				//email: "foo@foo.com"
 		})
 	}
