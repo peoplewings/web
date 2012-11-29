0e8e9803d000f7887f5bb7d6b919970036f9fddd
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-11-29 13:33:25 +0100

    Fixed pagination numbers

--- public/js/views/home/results.js
+++ public/js/views/home/results.js
@@ -20,12 +20,12 @@ define([
 		this.blurrStyle = (this.logged === false) ? 'style="color: transparent;text-shadow: 0 0 5px rgba(0,0,0,0.5)"' : ""
 	},
     render: function(results){
-		var tpl = _.template(resultsTpl, { blurrStyle: this.blurrStyle, results: results.profiles, startResult: "1", endResult: results.profiles.length, totalCount: results.count })
+		var tpl = _.template(resultsTpl, { blurrStyle: this.blurrStyle, results: results.profiles, startResult: results.startResult, endResult: results.endResult, totalCount: results.count })
 		this.$el.html(tpl);
 		$(this.targetEl).after(this.$el)
     },
 	nextPage: function(evt){
-		console.log("next", this.query.page)
+		//console.log("next", this.query.page)
 		var scope = this
 		this.query.page++
 		api.get(api.getApiVersion() + "/profiles", this.query, function(results){
@@ -34,7 +34,7 @@ define([
 		return false
 	},
 	previousPage: function(){
-		console.log("previous", this.query.page)
+		//console.log("previous", this.query.page)
 		var scope = this
 		this.query.page--
 		api.get(api.getApiVersion() + "/profiles", this.query, function(results){
