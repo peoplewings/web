fe2262cf570074ba2170bc5318fb165d623b8ba1
A. Matías Quezada <amatiasq@gmail.com>
2012-12-14 13:41:11 +0100

    Added comment to sample teste

--- public/js/test/core/api.test.js
+++ public/js/test/core/api.test.js
@@ -5,6 +5,9 @@ define(function(require) {
 	describe('API helper', function() {
 		it('should pass', function() {
 			expect(1).toBe(1);
+			//var server = sinon.fakeServer();
+			//api.get('/', spy1, spy2)
+			//expect(spy1.calledOnce).toBeTrue()
 		});
 		it('should fail', function() {
 			expect(1).toBe(2);
