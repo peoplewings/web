01387d332aa2569123344449456a24fabb5ac269
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-12-16 20:57:28 +0100

    Added tests for xHr calls on API helper

--- public/js/test/core/api.test.js
+++ public/js/test/core/api.test.js
@@ -1,7 +1,6 @@
 define(function(require) {
 
 	var api = require('api');
-	console.log(api)
 
 	describe('API helper', function() {
 		describe('#get method', function() {
@@ -21,7 +20,73 @@ define(function(require) {
 			it('should call the error callback', function() {
 				server.respondWith([400, { "Content-Type": "application/json" }, '[{ "status": false, "data": "Some data" }]'])
 				var callback = sinon.spy();
-				api.get("/", {}, {}, callback)
+				api.get("/", {}, null, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+		});
+		describe('#post method', function() {
+
+			var server;
+
+			before(function () { server = sinon.fakeServer.create(); });
+			after(function () { server.restore(); });
+			
+			it('should call the success callback', function() {
+				server.respondWith([200, { "Content-Type": "application/json" }, '[{ "status": true, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.post("/", {}, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+			it('should call the error callback', function() {
+				server.respondWith([400, { "Content-Type": "application/json" }, '[{ "status": false, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.post("/", {}, null, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+		});
+		describe('#put method', function() {
+
+			var server;
+
+			before(function () { server = sinon.fakeServer.create(); });
+			after(function () { server.restore(); });
+			
+			it('should call the success callback', function() {
+				server.respondWith([200, { "Content-Type": "application/json" }, '[{ "status": true, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.put("/", {}, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+			it('should call the error callback', function() {
+				server.respondWith([400, { "Content-Type": "application/json" }, '[{ "status": false, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.put("/", {}, null, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+		});
+		describe('#delete method', function() {
+
+			var server;
+
+			before(function () { server = sinon.fakeServer.create(); });
+			after(function () { server.restore(); });
+			
+			it('should call the success callback', function() {
+				server.respondWith([200, { "Content-Type": "application/json" }, '[{ "status": true, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.delete("/", callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+			it('should call the error callback', function() {
+				server.respondWith([400, { "Content-Type": "application/json" }, '[{ "status": false, "data": "Some data" }]'])
+				var callback = sinon.spy();
+				api.delete("/", null, callback)
 				server.respond()
 				expect(callback.calledOnce).toBeTrue()
 			});
