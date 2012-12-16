807e1b8bd77fc4131e79c63e0ab5d6282d31a10d
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-12-16 19:12:35 +0100

    First API helper unit tests

--- public/js/test/core/api.test.js
+++ public/js/test/core/api.test.js
@@ -3,14 +3,27 @@ define(function(require) {
 	var api = require('api');
 
 	describe('API helper', function() {
-		it('should pass', function() {
-			expect(1).toBe(1);
-			//var server = sinon.fakeServer();
-			//api.get('/', spy1, spy2)
-			//expect(spy1.calledOnce).toBeTrue()
-		});
-		it('should fail', function() {
-			expect(1).toBe(2);
+		describe('#get method', function() {
+
+			var server;
+
+			before(function () { server = sinon.fakeServer.create(); });
+			after(function () { server.restore(); });
+			
+			it('should call the success callback', function() {
+				server.respondWith([200, { "Content-Type": "text/html", "Content-Length": 2 }, "OK"])
+				var callback = sinon.spy();
+				api.get("/", {}, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
+			it('should call the error callback', function() {
+				server.respondWith([400, { "Content-Type": "text/html", "Content-Length": 2 }, "KO"])
+				var callback = sinon.spy();
+				api.get("/", {}, {}, callback)
+				server.respond()
+				expect(callback.calledOnce).toBeTrue()
+			});
 		});
 	});
 });
