930f80ddf6a458c4e3a5ecd9a649d005f2f8f491
A. Matías Quezada <amatiasq@gmail.com>
2012-12-18 17:36:57 +0100

    Developed notification row

--- public/js/lib/plugins/tmpl.js
+++ public/js/lib/plugins/tmpl.js
@@ -4,9 +4,38 @@ define(function(require) {
 	var Handlebars = require('handlebars');
 	var cache = [];
 
+	function createEnum(values) {
+		if (values instanceof Array)
+			values = _.object(_.values(values), _.keys(values));
+
+		values.fromValue = function(value) {
+			var result;
+			_.each(this, function(id, key) { if (id === value) result = key });
+			return result;
+		};
+
+		return values;
+	}
+
+	var enums = {
+		'notification-type': createEnum({
+			Request: 'requests',
+			Invitation: 'invitations',
+			Message: 'messages'
+		})
+	};
+
 	Handlebars.registerHelper('date', function(value, format) {
 		var date = typeof value === 'number' ? moment.unix(value) : moment(value);
+
+		if (format === 'from now')
+			return date.fromNow();
+
 		return date.format(typeof format === 'string' ? format : 'L')
+	});
+
+	Handlebars.registerHelper('enum', function(value, id) {
+		return enums[id].fromValue(value);
 	});
 
 	function load(name, parentRequire, done, config) {
