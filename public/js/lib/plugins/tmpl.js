28fb43df92e585174096986230aa10ea8cfd2f03
A. Matías Quezada <amatiasq@gmail.com>
2012-12-18 18:30:30 +0100

    Fixed Request/Invitation message

--- public/js/lib/plugins/tmpl.js
+++ public/js/lib/plugins/tmpl.js
@@ -39,12 +39,8 @@ define(function(require) {
 	});
 
 	function load(name, parentRequire, done, config) {
-		if (config.tmpl)
-			var file = ((config.tmpl.path + '/') || '') + name + (config.tmpl.extension || '');
-		else
-			var file = name;
-		text.load(file, parentRequire, function(template) {
-			done(cache[name] = Handlebars.compile(template));
+		text.load(name, parentRequire, function(template) {
+			done(Handlebars.compile(template));
 		}, config);
 	}
 
