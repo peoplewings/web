f034dbe1480e9050485ffe10fa602ac17263c1ce
A. Matías Quezada <amatiasq@gmail.com>
2012-12-20 10:52:00 +0100

    Renamed desktop.js => main.js and routers/desktopRouter.js => router.js

--- public/js/build/app.build.js
+++ public/js/build/app.build.js
@@ -1,14 +1,6 @@
-//This file is run in nodejs to do the build: node app.build.js
-
-//Load the requirejs optimizer
-var requirejs = require('./r.js');
-
-//Set up basic config, include config that is
-//common to all the requirejs.optimize() calls.
-var baseConfig = {
-
-  // Tells Require.js to look at desktop.js for all shim and path configurations
-  mainConfigFile: "desktop.js",
+({
+  // Tells Require.js to look at main.js for all shim and path configurations
+  mainConfigFile: "main.js",
 
   wrap: true,
 
@@ -21,42 +13,6 @@ var baseConfig = {
   // Uses uglify.js for minification
   optimize: "uglify",
 
- };
-
-//Create an array of build configs, the baseConfig will
-//be mixed into both the mobile and desktop builds below.
-
-var configs = [
-    {
-        include: ["mobile"],
-        out: "build/mobile.min.js"
-    },
-    {
-        include: ["desktop"],
-        out: "build/desktop.min.js"
-    },
-];
-
-
-// Function used to mix in baseConfig to a new config target
-function mix(target) {
-    for (var prop in baseConfig) {
-        if (baseConfig.hasOwnProperty(prop)) {
-            target[prop] = baseConfig[prop];
-        }
-    }
-    return target;
-}
-
-//Create a runner that will run a separate build for each item
-//in the configs array. Thanks to @jwhitley for this cleverness
-var runner = configs.reduceRight(function(prev, currentConfig) {
-  return function (buildReportText) {
-    requirejs.optimize(mix(currentConfig), prev);
-  };
-}, function(buildReportText) {
-    console.log(buildReportText);
-});
-
-//Run the builds
-runner();
+  include: ["main"],
+  out: "build/out.js"
+})
