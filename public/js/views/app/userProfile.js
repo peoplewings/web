1b06e4a82d64ed023bb29d1404c5f09e2640bced
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-12-20 14:01:35 +0100

    Working on user Profile

--- public/js/views/app/userProfile.js
+++ public/js/views/app/userProfile.js
@@ -1,48 +1,37 @@
 define(function(require){
 
 	var $ = require("jquery");
+	var Promise = require("promise");
 	var Backbone = require("backbone");
 	var api = require("api2");
-	var profileTpl = require("text!templates/app/preview.html");
-	var ProfilePreview = require("models/ProfilePreview")
+	var profileTpl = require("tmpl!templates/app/user-profile.html");
+	
 	
 	var userProfile = Backbone.View.extend({
 		el: "#main",
+		
 		initialize: function(userId){
-			this.model = new ProfilePreview({_id: userId })
-			this._modelBinder = new Backbone.ModelBinder();
-			this.render = this.render.bind(this)
-			this.setTemplate = this.setTemplate.bind(this)
-			
-			api.get('/api/v1/profiles/' + userId + '/preview')
-				.prop('data')
-				.then(this.setTemplate)
-				.then(this.render);
+			this.userId = userId
+			this.refresh = this.refresh.bind(this)
 		},
-		setTemplate: function(){
-			var tpl = _.template(previewTpl, {
-				birthday: bDay,
-				hometown: this.model.get("hometown"), 
-				avatar: this.model.get("avatar"), 
-				verified: this.model.get("verified"),
-				current: this.model.get("current"),
-				otherLocations: this.model.get("otherLocations"),
-				languages: this.model.get("languages"),
-				lastLoginDate: this.model.get("lastLoginDate"),
-				interestedIn: this.model.get("interestedIn")[0].gender, //Fix this shit please
-				education: this.model.get("education"),
-				wings: this.wingsList,
-				status_choices: {  Y: 'Yes', N: 'No', M: 'Maybe'},
-				sleep_choices: { C: "Common area", P: "Private area", S: "Shared private area"},
-				smoking_choices: { S: 'I smoke', D: "I don't smoke, but guests can smoke here", N: "No smoking allowed"}
+		
+		render: function(userId){
+			if (userId) this.userId = userId
+			Promise.parallel(
+				api.get('/api/v1/profiles/' + this.userId + '/preview'),
+				api.get('/api/v1/profiles/' + this.userId + '/accomodations/preview')
+			).spread(function(profile, wings) {
+				profile.data.wings = wings.data
+				return profile.data
+			}).then(this.refresh);
 
-			})
-			return tpl
 		},
-		render: function(tpl){
-			console.log(tpl)
-			$(this.el).html(tpl)
+		
+		refresh: function(data){
+			console.log(data)
+			$(this.el).html(profileTpl(data))
 		},
+		
 		destroy: function(){
 			this.remove()
 			this.unbind()
