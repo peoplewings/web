2badc067bf3087bfc49bee366e075dc130b58650
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-01-03 16:47:29 +0100

    Refactored User model to Account model. save, destroy methods return future promises. save, destroy methods use custom API. fetch uses default $.ajax(), more refactor needed

--- public/js/models/Account.js
+++ public/js/models/Account.js
@@ -6,21 +6,42 @@ define(function(require) {
 
 	var UserModel = Backbone.Model.extend({
 
-		//urlRoot: api.getServerUrl() + api.getApiVersion() + '/accounts/',
+		urlRoot: api.getServerUrl() + api.getApiVersion() + '/accounts/',
 		
-		fetch: function(args) {
-			console.log("jdhfjkehj")
-            api.get('/accounts/', {}, this.parse)
+		/*
+		fetch: function(options) {
+            api.get(api.getApiVersion() + '/accounts/' + this.id, {}, this.parse)
         },
+		*/
+
+		save: function(attributes, pwd) {
+			var sc = this
+			return api.put(api.getApiVersion() + '/accounts/' + this.id, { resource: attributes, currentPassword: pwd })
+				.prop('status')
+				.then(function(status){
+					if (status === true){
+						for (attr in attributes) 
+							sc.set(attr, attributes[attr]);
+					}
+					return status;
+				})
+		},
 
 		parse: function(resp, xhr) {
 			return resp.data
 		},
 		
-		destroy: function(options) {
-			console.log(this.id)
-			debugger;
-			api.delete(this.urlRoot + this.id, options.data, options.cb)
+		destroy: function(data) {
+			var sc = this
+			return api.delete(api.getApiVersion() + '/accounts/' + this.id, data)
+					.prop('status')
+					.then(function(status){
+						if (status === true){
+							for (attr in sc.attributes) 
+								sc.unset(attr);
+							}
+							return status;
+						})
 		}
 	});
 
