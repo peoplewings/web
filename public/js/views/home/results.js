d72efa092ee30f5675bffdef0055e16185300af5
A. Matías Quezada <amatiasq@gmail.com>
2013-01-03 18:56:26 +0100

    Developed new message modal

--- public/js/views/home/results.js
+++ public/js/views/home/results.js
@@ -1,54 +1,98 @@
-define([
-  'jquery',
-  'backbone',
-  'utils',
-  'api',
-  'text!templates/home/search_result.html',
-], function($, Backbone, utils, api, resultsTpl){
+define(function(require){
 
-  var resultsView = Backbone.View.extend({
-	//el: "#search-results",
-	events: {
-		"click button.fake-btn": "alertLog",
-		"click a.nextPage": "nextPage",
-		"click a.previousPage": "previousPage"
-	},
-	initialize: function(options){
-		this.targetEl = options.target
-		this.logged = options.logged
-		this.query = options.query
-		this.blurrStyle = (this.logged === false) ? 'style="color: transparent;text-shadow: 0 0 5px rgba(0,0,0,0.5)"' : ""
-	},
-    render: function(results){
-		var tpl = _.template(resultsTpl, { blurrStyle: this.blurrStyle, results: results.profiles, startResult: results.startResult, endResult: results.endResult, totalCount: results.count })
-		this.$el.html(tpl);
-		$(this.targetEl).after(this.$el)
-    },
-	nextPage: function(evt){
-		//console.log("next", this.query.page)
-		var scope = this
-		this.query.page++
-		api.get(api.getApiVersion() + "/profiles", this.query, function(results){
-			scope.render(results.data)
-		})
-		return false
-	},
-	previousPage: function(){
-		//console.log("previous", this.query.page)
-		var scope = this
-		this.query.page--
-		api.get(api.getApiVersion() + "/profiles", this.query, function(results){
-			scope.render(results.data)
-		})
-		return false
-	},
-	close: function(){
-		this.remove()
-		this.unbind()
-	},
-	alertLog: function(){
-		alert("You need to be logged in to use this function")
-	}
-  });
-  return resultsView;
+	var $ = require('jquery');
+	var Backbone = require('backbone');
+	var utils = require('utils');
+	var api = require('api');
+	var UserAccount = require('models/User');
+	var resultsTpl = require('tmpl!templates/home/search_result.html');
+	var modalTpl = require('tmpl!templates/lib/modal2.html');
+	var sendMessageTpl = require('tmpl!templates/lib/send-message.html');
+
+	var resultsView = Backbone.View.extend({
+		//el: "#search-results",
+		events: {
+			"click button.send-message-btn": "sendMessage",
+			"click button.fake-btn": "alertLog",
+			"click a.nextPage": "nextPage",
+			"click a.previousPage": "previousPage"
+		},
+
+		initialize: function(options){
+			this.namesById = {};
+			this.targetEl = options.target
+			this.logged = options.logged
+			this.query = options.query
+			this.blurrStyle = (this.logged === false) ? 'style="color: transparent;text-shadow: 0 0 5px rgba(0,0,0,0.5)"' : ""
+		},
+	    render: function(results){
+	    	var self = this;
+
+			this.$el.html(resultsTpl({
+				blurrStyle: this.blurrStyle,
+				startResult: results.startResult,
+				endResult: results.endResult,
+				totalCount: results.count,
+				results: results.profiles.map(function(result) {
+					result.id = result.resourceUri.split("/")[4];
+					self.namesById[result.id] = result.firstName + ' ' + result.lastName;
+					return result;
+				})
+			}));
+			$(this.targetEl).after(this.$el)
+	    },
+		nextPage: function(evt){
+			//console.log("next", this.query.page)
+			var scope = this
+			this.query.page++
+			api.get(api.getApiVersion() + "/profiles", this.query, function(results){
+				scope.render(results.data)
+			})
+			return false
+		},
+		previousPage: function(){
+			//console.log("previous", this.query.page)
+			var scope = this
+			this.query.page--
+			api.get(api.getApiVersion() + "/profiles", this.query, function(results){
+				scope.render(results.data)
+			})
+			return false
+		},
+		close: function(){
+			this.remove()
+			this.unbind()
+		},
+		alertLog: function(){
+			alert("You need to be logged in to use this function")
+		},
+
+		pepe: function(event) {
+			this.sendMessage(event);
+		},
+
+		sendMessage: function(event) {
+			var avatar = new UserAccount({ id: api.getUserId() }).get('avatar');
+
+			var modal = $(modalTpl({
+				modalHeader: "New message",
+				acceptBtn: "Send",
+			}));
+			$("body section:last").append(modal);
+
+			var id = $(event.target).parents('.search-result').data('profile-id');
+			var name = this.namesById[id];
+
+			modal.find('div.modal-body').html(sendMessageTpl({
+				avatar: avatar,
+				to: {
+					id: id,
+					fullname: name,
+				}
+			}));
+
+			modal.modal('show');
+		}
+	});
+	return resultsView;
 });
