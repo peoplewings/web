79b4639fe569429e2f350a141797b7527e83120d
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-01-03 16:57:39 +0100

    Refactored settings view. Templates use handlebars.js. View uses new Account model. Data binding between view and model.

--- public/js/views/app/settings.js
+++ public/js/views/app/settings.js
@@ -5,11 +5,13 @@ define(function(require){
 	var api = require("api2");
 	var utils = require("utils");
 	var settingsTpl = require("tmpl!templates/app/settings.html")
-	var alertTpl = require("text!templates/lib/alert.html")
+	var alertTpl = require("tmpl!templates/lib/alert.html")
 	var UserModel = require("models/Account")
 	
 	var settingsView = Backbone.View.extend({
+		
     	el: "#main",
+
 		events:{
 			"submit form#settings-form": "submitSettings",
 			"click a[href='#myModal']": function(){ 
@@ -18,10 +20,12 @@ define(function(require){
 									},
 			"click button#delete-account-btn": "deleteAccount",
 		},
+		
 		initialize: function(options){
 			this.model = new UserModel({id:api.getUserId()})
 			this.model.on("change", this.render.bind(this));
 		},
+		
 	    render: function(){
 			$(this.el).html(settingsTpl(this.model.toJSON()));
 		
@@ -46,51 +50,50 @@ define(function(require){
 				}
 			})
 	    },
+	
 		submitSettings: function(e){
 			e.preventDefault(e);
+			var tpl = null
 			var data = utils.serializeForm('settings-form')
 			var values = { firstName: data.firstName, lastName: data.lastName }
 		
 			if (data.newEmail) values.email = data.newEmail
 			if (data.newPassword) values.password = data.newPassword
 				
-			var sc = this
-			api.put(api.getApiVersion() + '/accounts/' + api.getUserId(), { resource: values, currentPassword: data.password }, function(response){ 
-				$('.alert').remove()
-				var tpl
-				if (response.status === true) {
-					tpl = _.template(alertTpl, {extraClass: 'alert-success', heading: response.msg})
-				}else tpl = _.template(alertTpl, {extraClass: 'alert-error', heading: response.msg + ": ", message: 'Please try again later'})
-				$('#settings-form')[0].reset()
-
-				for (val in values) sc.model.set(val, values[val])
-			
-				$('#firstName').val(sc.model.get("firstName"))
-				$('#lastName').val(sc.model.get("lastName"))
-			
-				$('#main').prepend(tpl)
-			
-			})
+			this.model.save(values, data.password)
+				.then(function(status){
+					if (status === true){
+						tpl = alertTpl({extraClass: 'alert-success', heading: "Account updated"})
+					} else {
+						tpl = alertTpl({extraClass: 'alert-error', heading: "Account couldn't be updated" + ": ", message: 'Please try again later'})
+					}
+				})
+				.fin(function(){
+					$('#settings-form')[0].reset()
+					$("#main").prepend(tpl)
+				})
 		},
+		
 		deleteAccount: function(){
-			var data = utils.serializeForm('delete-account-form')
-			var goodbye = function(resp){
-				if (resp.status === true) {
-					$('#myModal').modal('hide')
-					require(["views/app/logout"], function(logoutView){
-						logoutView.goodbye()
+			if ($('#delete-account-form').valid()){
+				var data = utils.serializeForm('delete-account-form')
+				
+				this.model.destroy(data)
+					.then(function(status){
+						if (status === true){
+							$('#myModal').modal('hide')
+							require(["views/app/logout"], function(logoutView){
+								logoutView.goodbye()
+							})
+						} else {
+							$('.alert').remove()
+							var	tpl = alertTpl({extraClass: 'alert-error', heading: "Couldn't delete your account" + ": ", message: 'Please retype your password'})
+							$('#myModal > .modal-body').prepend(tpl)
+							$('#delete-account-form')[0].reset()
+						}
 					})
-				}
-				else{
-					$('.alert').remove()
-					var tpl = _.template(alertTpl, {extraClass: 'alert-error', heading: resp.msg + ": ", message: 'Please retype your password'})
-					$('#myModal > .modal-body').prepend(tpl)
-					//console.log(resp.msg)
-				} 
-			}
-			if ($('#delete-account-form').valid())
-				this.model.destroy({data: data, cb: goodbye })
-				//api.delete(api.getApiVersion() + '/accounts/' + api.getUserId(), data, goodbye)
+			} else 
+				return;
 		}
 	});
 	
