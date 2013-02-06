d691ef471dc99a8e4a2497b2a575be0a44324648
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-02-06 13:52:48 +0100

    Refactor template to handlebars style. Added terms and conditions checkbox.

--- public/js/views/home/register.js
+++ public/js/views/home/register.js
@@ -1,68 +1,108 @@
 define(function(require) {
 
-    var $ = require('jquery');
-    var Backbone = require('backbone');
-    var api = require('api2');
-    var utils = require('utils');
-    var handlersV = require('views/lib/handlers');
-    var responseView = require('views/lib/response');
-    var registerTpl = require('tmpl!templates/home/register.html')
+	var $ = require('jquery');
+	var Backbone = require('backbone');
+	var api = require('api2');
+	var utils = require('utils');
+	var phrases = require("phrases");
+	
+	var responseView = require('views/lib/response');
 
-    var registerView = Backbone.View.extend({
+	var registerTpl = require('tmpl!templates/home/register.html');
+	var alertTpl = require('tmpl!templates/lib/alert.html');
 
-        el: "#main",
+	var registerView = Backbone.View.extend({
 
-        events: {
-            "submit form#register-form": "submitRegister"
-        },
+		el: "#main",
 
-        validation: {
-            rules: {
-                password: {
-                    minlength: 8,
-                    validpassword: true
-                },
-                birthdayYear: {
-                    max: (new Date()).getFullYear() - 18,
-                },
-                email: {
-                    email: true
-                },
-                repeatEmail: {
-                    email: true,
-                    equalTo: '#email'
-                },
-            },
-            messages: {
-                birthdayYear: {
-                    max: "You need to be +18 years old to register"
-                }
-            },
-            errorPlacement: function(error, element) {
-                error.appendTo(element.nextAll("span.help-block"));
-            },
-        },
+		events: {
+			"submit form#register-form": "submitRegister"
+		},
 
-        render: function() {
+		validation: {
+			rules: {
+				password: {
+					minlength: 8,
+					validpassword: true
+				},
+				birthdayYear: {
+					max: (new Date()).getFullYear() - 18,
+				},
+				email: {
+					email: true
+				},
+				repeatEmail: {
+					email: true,
+					equalTo: '#email'
+				},
+				terms: {
+					required: true,
+				}
+			},
+			messages: {
+				birthdayYear: {
+					max: "You need to be +18 years old to register"
+				},
+				has_accepted_terms: "Please accept our terms"
+			},
+			errorPlacement: function(error, element) {
+				error.appendTo(element.nextAll("span.help-block"));
+			},
+		},
 
-            $(this.el).html(registerTpl);
+		render: function() {
 
-            $('#register-form').validate(this.validation)
+			$(this.el).html(registerTpl({
+				month: phrases.months,
+			}));
 
-        },
+			this.$('#register-form').validate(this.validation);
 
-        submitRegister: function(e) {
-            e.preventDefault(e);
-            var data = utils.serializeForm(e.target.id)
-            handlersV.submitForm(e.target.id, api.getApiVersion() + '/newuser', data, responseView,
-            {
-                legend: "Confirm your e-mail address",
-                msg: "A confirmation email has been sent to ",
-                extraInfo: data.email
-            })
-        }
+		},
 
-    });
+		submitRegister: function(e) {
+			e.preventDefault(e);
 
-    return new registerView;
+			var self = this;
+			var spinner = new Spinner(utils.getSpinOpts());
+			var data = utils.serializeForm(e.target.id);
+
+			if (data.has_accepted_terms === "on") 
+				data.has_accepted_terms = true;
+			else {
+				console.error("Hmm...");
+				return;
+			}
+
+			spinner.spin(document.getElementById('main'));
+
+			api.post(api.getApiVersion() + '/newuser', data)
+			.then(function(response){
+				spinner.stop()
+				if(response.status === true) {
+
+					self.$('#register-form').remove();
+					responseView.extraData = "Code: " + response.code + " - " + response.msg;
+					responseView.render({
+						legend: "Confirm your e-mail address",
+						msg: "A confirmation email has been sent to ",
+						extraInfo: data.email
+					})
+
+					$(self.el).html(responseView.el);
+
+				} else {
+					debugger
+					$(self.el).prepend(alertTpl( {
+						extraClass: 'alert-error',
+						heading: "Code: " + response.code + ". Error: ",
+						message: JSON.stringify(response.errors) || response.error,
+					}))
+				}
+			});
+		}
+
+	});
+
+	return new registerView;
 });
