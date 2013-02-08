870a1fb93e5d07463654eb96ea9b2245bad18185
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-02-08 14:19:08 +0100

    Implemented Deny/Cancel in a thread

--- public/js/views/app/thread.js
+++ public/js/views/app/thread.js
@@ -31,12 +31,12 @@ define(function(require) {
 			'click #send-response': 'sendResponse',
 			'click #cancel-response': 'cancelResponse',
 
-			'click .option-chat': 'reply',
-			'click .option-accept': 'optAccept',
-			'click .option-maybe': 'optMaybe',
-			'click .option-reopen': 'optReopen',
-			'click .option-deny': 'optDeny',
-			'click .option-cancel': 'optDeny',
+			'click .option-Chat': 'reply',
+			'click .option-Accept': 'optAccept',
+			'click .option-Maybe': 'optMaybe',
+			'click .option-Reopen': 'optReopen',
+			'click .option-Deny': 'optDeny',
+			'click .option-Cancel': 'optDeny',
 		},
 
 		initialize: function() {
@@ -59,12 +59,18 @@ define(function(require) {
 				).spread(function(prevThread, nextThread) {
 					return api.get('/api/v1/notificationsthread/' + id)
 						.prop('data')
+						.then(function(data){
+							self.parseOptions(data.options, data.firstSender, data.wing.state)
+							self.data = data;
+							return data;
+						})
 						.then(self.refresh.bind(self, prevThread, nextThread));
 				});
 			});
 		},
 
 		refresh: function(prevThread, nextThread, data) {
+			var defaultOptions = ["Accept", "Pending", "Maybe", "Deny"];
 			var last = data.items.pop();
 			var isMessage = data.kind === 'message';
 			var parameters = null
@@ -95,16 +101,19 @@ define(function(require) {
 					avatar: item.senderSmallAvatar,
 					connected: item.senderConnected,
 					content: item.content.message,
+					state: data.wing.state,
+					//flagDirection: data.firstSender !== api.getUserId()
 				},
 				{
 					wingParameters: parameters
 				});
 			});
 
-			function openTpl(item) {
+			function openTpl(item, defaultOptions) {
 				return openItemTpl(item, data.wing, {
 					isMessage: isMessage,
-					options: [ 'accept', 'chat', 'maybe', 'reopen', 'cancel', 'deny' ]
+					options: data.options,
+					defaultOptions: defaultOptions,
 				});
 			}
 
@@ -117,17 +126,18 @@ define(function(require) {
 			this.$el.html(threadTpl(data, {
 				isMessage: isMessage,
 				iStarted: data.firstSender === api.getUserId(),
-				options: [ 'accept', 'chat', 'maybe', 'reopen', 'cancel', 'deny' ],
+				options: data.options,
 				previous: prevThread,
 				next: nextThread,
+				parameters: data.wing.parameters,
 				me: { avatar: avatar },
-				items: items.map(itemTpl).join('') + openTpl(last),
+				items: items.map(itemTpl).join('') + openTpl(last, defaultOptions),
 			}));
 
 			var allButLast = Array.prototype.slice.call(self.$('#notif-list').children(), 0, -2);
 			$(allButLast).click(function openItem() {
 				var closed = $(this);
-				var open = $(openTpl(data.items[$(this).data('index')]));
+				var open = $(openTpl(data.items[$(this).data('index')], defaultOptions));
 
 				$(this).replaceWith(open);
 				open.click(function closeItem() {
@@ -174,6 +184,16 @@ define(function(require) {
 					.focus();
 		},
 
+		editWingParams: function(){
+			this.$('#response-options').hide();
+			this.$('#write-response')
+				.show()
+				.find('textarea')
+					.focus();
+			this.$('#edit-wing-params')
+				.show();
+		},
+
 		cancelResponse: function() {
 			this.$('#response-options').show();
 			this.$('#write-response')
@@ -183,14 +203,19 @@ define(function(require) {
 		},
 
 		sendResponse: function() {
-			var content = this.$('#write-response textarea').val();
-			if (!content)
+			var resp = { content: this.$('#write-response textarea').val() };
+			if (!resp.content)
 				return Promise.resolved(false);
+
+			if (this.data.kind === "request" || this.data.kind === "invite"){
+				resp.wingParameters = this.getWingParameters();
+				resp.state = this.getWingState();
+			}
 
 			var self = this;
 			return api.post('/api/v1/notificationsthread/', {
 				reference: this.current.id,
-				data: { content: content }
+				data: resp,
 			}).then(function() {
 				return self.render(self.current.id);
 			});
@@ -209,8 +234,34 @@ define(function(require) {
 		},
 
 		optDeny: function() {
-			console.log('deny');
+			this.reply();
+			
+			this.$('#write-response > .params-box')
+			.show();
+
+			this.data.wing.state = "D";
 		},
+
+		parseOptions: function(options, firstSender, wingState){
+			if (api.getUserId() == firstSender)
+				options[options.indexOf("Deny")] = "Cancel";
+				if (wingState == "D")
+					options[options.indexOf("Pending")] = "Reopen";
+		},
+
+		getWingParameters: function(){
+			return {
+				startDate: this.data.wing.parameters.startDate,
+				endDate: this.data.wing.parameters.endDate,
+				capacity: this.data.wing.parameters.capacity,
+				flexibleStartDate: this.data.wing.parameters.flexibleStartDate,
+				flexibleEndDate: this.data.wing.parameters.flexibleEndDate,
+			};
+		},
+
+		getWingState: function(){
+			return this.data.wing.state;
+		}
 	});
 
 	return new threadView;
