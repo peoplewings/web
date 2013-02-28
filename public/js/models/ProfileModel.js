7ea42914817c6a1e959f305f5bce25bd2cd8c838
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-02-28 14:38:17 +0100

    Refactor PROFILE template and view ctrl. ProfileView ctrl handles READ only profile. ProfileModel holds READ only profile.

---
+++ public/js/models/ProfileModel.js
@@ -0,0 +1,28 @@
+define(function(require) {
+
+	var Backbone = require('backbone');
+	var api = require('api2');
+	var phrases = require('phrases');
+	var factory = require('core/factory');
+
+	var Preview = Backbone.Model.extend({
+
+		urlRoot: api.getServerUrl() + api.getApiVersion() + "/profiles/",
+
+		url: function(){
+			return this.urlRoot + this.id + "/preview"
+		},
+
+		parse: function(resp, xhr){
+			
+			resp.data.replyTime = moment.duration(resp.data.replyTime).humanize();
+			resp.data.civilState = phrases.choices["civilState"][resp.data.civilState];
+
+			return resp.data
+		},
+
+	});
+
+    // Returns the Model singleton instance
+	return factory(Preview);
+});
