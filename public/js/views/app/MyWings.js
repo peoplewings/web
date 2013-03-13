4740f8c448566aa6fcf956849ea0b1102f3c3d5c
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-03-13 14:05:38 +0100

    Refactor wings tab controller. Open wing edition and change status work.

--- public/js/views/app/MyWings.js
+++ public/js/views/app/MyWings.js
@@ -3,67 +3,33 @@ define(function(require) {
 	var $ = require("jquery");
 	var Backbone = require("backbone");
 	var api = require("api2");
-	var Promise = require('promise');
-	var wingsTpl = require("tmpl!templates/app/wings.html");
 	var alerts = require('views/lib/alerts');
-	var WingView = require("views/app/wing");
-	var ProfileModel = require("models/ProfileModel");
+	var wingFormTpl = require("tmpl!templates/app/profile.form.wings.html");
 
 	var WingsView = Backbone.View.extend({
 
 		el: "#main",
 
 		events: {
+			"change [name=generalStatus]": "changeStatus",
 			"click button[id^=edit-wing-]": "editWing",
 		},
 
-		initialize: function() {
-			/*
-			this.list = new Backbone.Collection();
-			this.list.on("reset", this.refresh.bind(this));
-			*/
-			console.log(this.events)
-
+		initialize: function(parent) {
+			this.parentCtrl = parent;
 		},
 
 		editWing: function(evt){
+			var wingId = $(evt.target).parent().attr("id") || evt.target.id;
+			wingId = +wingId.split("wing-")[1];
 
-			var wingId = $(evt.target).parent().id || evt.target.id;
-
-			debugger;
-			console.log($(evt.target), wingId.split("wing-")[1]);
-
-		},
-
-		render: function(wingId) {
-			var self = this;
-
-			Promise.parallel(
-				api.get(api.getApiVersion() + "/profiles/" + api.getUserId()),
-				api.get(api.getApiVersion() + "/profiles/" + api.getUserId() + "/accomodations/list")
-			).spread(function(profile, wings) {
-				self.model.set(profile.data, {silent: true});
-				self.list.reset(wings.data);
-				if (wingId)
-					self.openWing({ id: wingId, update: true});
+			var wing = _.find(this.parentCtrl.model.get("wingsCollection"), function(wing){
+				return wing.id === wingId;
 			});
-		},
-
-		refresh: function() {
-			$(this.el).html(wingsTpl({
-				generalStatus: this.model.get("pwState"),
-				wings: this.list.toJSON(),
-			}));
-		},
-
-		openWing: function(options){
-			this.wingView = new WingView({
-				papa: this,
-				id: options.id,
-				update: options.update,
-			});
-
-			this.wingView.render(options.update);
+			var box = this.$("#wing-box-" + wingId);
+			console.log(wing)
+			$(box).html(wingFormTpl(wing));
+			
 		},
 
 		changeStatus: function(e) {
@@ -76,18 +42,6 @@ define(function(require) {
 				alerts.defaultError();
 			});
 
-		},
-
-		addWingToList: function(wing) {
-			this.list.push(wing);
-			this.render();
-		},
-
-		deleteWingFromList: function(uri) {
-			this.list = _.reject(this.list, function(wing) {
-				return wing.uri === uri;
-			});
-			this.render();
 		},
 	});
 
