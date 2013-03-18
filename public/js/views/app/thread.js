c8a155794878d2b9b620b1e10dcb15125cd7a54b
Miguel Ángel Ríos <miguelangel@peoplewings.com>
2013-03-18 19:12:15 +0100

    FIX PWF-338 resolved status wings in open notification

--- public/js/views/app/thread.js
+++ public/js/views/app/thread.js
@@ -229,7 +229,7 @@ define(function(require) {
 				this.$('.state-flag')
 					.removeClass(e(this.data.wing.state, "notification-state"))
 					.addClass(e(this.prevState, "notification-state"))
-					.find("span")
+					.find("h2")
 					.text(e(this.prevState, "notification-state"));
 
 				this.data.wing.state = this.prevState;
@@ -301,7 +301,7 @@ define(function(require) {
 				.show()
 				.removeClass(e(prevState, "notification-state"))
 				.addClass(e(option, "notification-state"))
-				.find("span")
+				.find("h1")
 				.text(e(option, "notification-state"));
 
 			this.$("#wing-params-form")
