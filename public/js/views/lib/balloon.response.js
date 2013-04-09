7ab7b5ef9297b6813f36636ffb5448777e05fcfc
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-04-09 14:31:10 +0200

    Refactored balloon response views controller.

---
+++ public/js/views/lib/balloon.response.js
@@ -0,0 +1,15 @@
+define(function(require) {
+
+	var $ = require('jquery');
+	var Backbone = require('backbone');
+	var balloonTpl = require('tmpl!templates/lib/balloon.html');
+
+	var BalloonView = Backbone.View.extend({
+
+		render: function(options){
+			$(this.el).html(balloonTpl({options: options}));
+		}
+	});
+
+	return new BalloonView;
+});
