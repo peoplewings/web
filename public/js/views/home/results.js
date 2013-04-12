536c53871a68080ff31d7b7c7c4d972248cd0a2a
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2013-04-12 13:30:52 +0200

    Cleanup code.

--- public/js/views/home/results.js
+++ public/js/views/home/results.js
@@ -1,4 +1,4 @@
-define(function(require){
+define(function(require) {
 
 	var $ = require('jquery');
 	var Backbone = require('backbone');
@@ -19,15 +19,15 @@ define(function(require){
 			"click a.previousPage": "previousPage"
 		},
 
-		initialize: function(options){
+		initialize: function(options) {
 			this.namesById = {};
 			this.logged = options.logged;
 			this.query = options.query;
 		},
-		setQuery: function(query){
+		setQuery: function(query) {
 			this.query = query;
 		},
-		render: function(results){
+		render: function(results) {
 			var self = this;
 
 			this.$el.html(resultsTpl({
@@ -42,43 +42,41 @@ define(function(require){
 					result.online = (result.online === 'ON') ? 'Online' : false;
 					result.id = result.profileId;
 					result.replyTime = utils.formatReplyTime(+result.replyTime);
-					self.namesById[result.id] = result.firstName + ' ' + result.lastName;
+					self.namesById[result.id]  = result.firstName + ' ' + result.lastName;
 					return result;
 				})
 			}));
 
-			this.lastPage = results.count/results.endResult;
+			this.lastPage = results.count / results.endResult;
 
-			$('[id^=collapse]').on('hide', function () {
-  				self.toggleCollapsibleText(this, 'less', 'more');
+			$('[id^=collapse]').on('hide', function() {
+				self.toggleCollapsibleText(this, 'less', 'more');
 			});
 
-			$('[id^=collapse]').on('show', function () {
-  				self.toggleCollapsibleText(this, 'more', 'less');
+			$('[id^=collapse]').on('show', function() {
+				self.toggleCollapsibleText(this, 'more', 'less');
 			});
 
 		},
-		nextPage: function(){
+		nextPage: function() {
 			var scope = this;
-			if (+this.query.page === this.lastPage)
-				return false;
+			if (+this.query.page === this.lastPage) return false;
 			this.query.page++;
-			api.get(api.getApiVersion() + "/profiles", this.query).then(function(results){
+			api.get(api.getApiVersion() + "/profiles", this.query).then(function(results) {
 				scope.render(results.data);
 			});
 			return false;
 		},
-		previousPage: function(){
+		previousPage: function() {
 			var scope = this;
-			if (+this.query.page === 1)
-				return false;
+			if (+this.query.page === 1) return false;
 			this.query.page--;
-			api.get(api.getApiVersion() + "/profiles", this.query).then(function(results){
+			api.get(api.getApiVersion() + "/profiles", this.query).then(function(results) {
 				scope.render(results.data);
 			});
 			return false;
 		},
-		close: function(){
+		close: function() {
 			this.remove();
 			this.unbind();
 		},
@@ -101,9 +99,9 @@ define(function(require){
 			notifications.invitation(id, name);
 		},
 
-		toggleCollapsibleText: function(target, targetText, replaceText){
+		toggleCollapsibleText: function(target, targetText, replaceText) {
 			var heading = $(target).next('.accordion-heading').find('h1');
-			var re = new RegExp('(' + targetText+ ')');
+			var re = new RegExp('(' + targetText + ')');
 			var changed = heading.html().replace(re, replaceText);
 			heading.html(changed);
 		}
