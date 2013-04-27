10d0ffa1e916ca90f3df5de210148c651b5f898e
A. Matías Quezada <amatiasq@gmail.com>
2013-04-27 19:10:43 +0200

    Fixed. Was commented by Miguel Angel because the code was hidding the element always.

--- public/js/views/app/notifications.js
+++ public/js/views/app/notifications.js
@@ -92,7 +92,8 @@ define(function(require){
 
 			this.$('#notification-sender').delegate('input', 'change', this.filter.bind(this));
 			this.$('select#ri-filters').on('change', this.filter.bind(this));
-			// this.$('#ri-status').on('change', this.filter.bind(this)).hide(); Sergi, here !
+			this.$('#ri-status').on('change', this.filter.bind(this));
+
 			return this.loadData(filters).then(this.refresh);
 		},
 
