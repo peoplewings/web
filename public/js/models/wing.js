9b76431a827ca283418b93cf8996989436c8216e
A. Matías Quezada <amatiasq@gmail.com>
2013-08-07 12:58:20 +0200

    Renaming /models/wing.js case

---
+++ public/js/models/wing.js
@@ -0,0 +1,22 @@
+define(function(require) {
+
+	var Backbone = require('backbone');
+	var api = require('api');
+	var factory = require('core/factory');
+
+	var Wing = Backbone.Model.extend({
+
+		urlRoot: api.getApiVersion() + '/wings/',
+
+		url: function() {
+			return this.urlRoot + (this.id ? this.id + '/' : '');
+		},
+
+		parse: function(response) {
+			return response.id ? response : response.data;
+		}
+	});
+
+	// Returns the Model singleton instance
+	return factory(Wing);
+});
